5ee9fb9bf2449ca127ff7de876a1b2e85e215e8e
Dale Harvey <dale@arandomurl.com>
2012-11-27 13:26:16 +0000

    Add etags to served manifests to help test updates

--- app.js
+++ app.js
@@ -1,75 +1,86 @@
 var express = require('express');
 var fs = require('fs');
+var crypto = require('crypto');
 
 var app = express();
 
 // Serve special files
 app.get('/manifest.webapp', function(req, res) {
   var manifest = fs.readFileSync('manifest.webapp').toString();
-
+  var hash = crypto.createHash('md5').update(manifest).digest("hex");
+  res.set('Etag', hash);
   res.writeHead(200, {'Content-Type': 'application/x-web-app-manifest+json'});
   res.end(manifest);
 });
 
 app.get('/manifest.appcache', function(req, res) {
   var manifest = fs.readFileSync('manifest.appcache').toString();
-
+  var hash = crypto.createHash('md5').update(manifest).digest("hex");
+  res.set('Etag', hash);
   res.writeHead(200, {'Content-Type': 'text/cache-manifest'});
   res.end(manifest);
 });
 
 app.get('/apps/bigHostedApp/manifest.webapp', function(req, res) {
   var manifest = fs.readFileSync(__dirname + '/webapp/apps/bigHostedApp/manifest.webapp').toString();
-
+  var hash = crypto.createHash('md5').update(manifest).digest("hex");
+  res.set('Etag', hash);
   res.writeHead(200, {'Content-Type': 'application/x-web-app-manifest+json'});
   res.end(manifest);
 });
 
 app.get('/apps/bigHostedApp/manifest.appcache', function(req, res) {
   var manifest = fs.readFileSync(__dirname + '/webapp/apps/bigHostedApp/manifest.appcache').toString();
-
+  var hash = crypto.createHash('md5').update(manifest).digest("hex");
+  res.set('Etag', hash);
   res.writeHead(200, {'Content-Type': 'text/cache-manifest'});
   res.end(manifest);
 });
 
 app.get('/apps/hostedAppEp/manifest.webapp', function(req, res) {
   var manifest = fs.readFileSync(__dirname + '/webapp/apps/hostedAppEp/manifest.webapp').toString();
-
+  var hash = crypto.createHash('md5').update(manifest).digest("hex");
+  res.set('Etag', hash);
   res.writeHead(200, {'Content-Type': 'application/x-web-app-manifest+json'});
   res.end(manifest);
 });
 
 app.get('/apps/packagedApp/manifest.webapp', function(req, res) {
   var manifest = fs.readFileSync(__dirname + '/webapp/apps/packagedApp/manifest.webapp').toString();
-
+  var hash = crypto.createHash('md5').update(manifest).digest("hex");
+  res.set('Etag', hash);
   res.writeHead(200, {'Content-Type': 'application/x-web-app-manifest+json'});
   res.end(manifest);
 });
 
 app.get('/apps/bigPackagedApp/manifest.webapp', function(req, res) {
   var manifest = fs.readFileSync(__dirname + '/webapp/apps/bigPackagedApp/manifest.webapp').toString();
-
+  var hash = crypto.createHash('md5').update(manifest).digest("hex");
+  res.set('Etag', hash);
   res.writeHead(200, {'Content-Type': 'application/x-web-app-manifest+json'});
   res.end(manifest);
 });
 
 app.get('/apps/packagedAppEp/manifest.webapp', function(req, res) {
   var manifest = fs.readFileSync(__dirname + '/webapp/apps/packagedAppEp/manifest.webapp').toString();
-
+  var hash = crypto.createHash('md5').update(manifest).digest("hex");
+  res.set('Etag', hash);
   res.writeHead(200, {'Content-Type': 'application/x-web-app-manifest+json'});
   res.end(manifest);
 });
 
 app.get('/apps/appWrongCache/manifest.webapp', function(req, res) {
   var manifest = fs.readFileSync(__dirname + '/webapp/apps/appWrongCache/manifest.webapp').toString();
-
+  var hash = crypto.createHash('md5').update(manifest).digest("hex");
+  res.set('Etag', hash);
   res.writeHead(200, {'Content-Type': 'application/x-web-app-manifest+json'});
   res.end(manifest);
 });
 
 app.get('/apps/appWrongCache/manifest.appcache', function(req, res) {
   var manifest = fs.readFileSync(__dirname + '/webapp/apps/appWrongCache/manifest.appcache').toString();
-
+  var hash = crypto.createHash('md5').update(manifest).digest("hex");
+  res.set('Etag', hash);
   res.writeHead(200, {'Content-Type': 'text/cache-manifest'});
   res.end(manifest);
 });
